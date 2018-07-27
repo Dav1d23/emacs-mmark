96174bb9f9e2914b05b9d02151dbc4d3543a60a5
David <petrodavi@gmail.com>
2018-07-27 08:20:22 +0200

    small changes

--- mmark.el
+++ mmark.el
@@ -20,52 +20,33 @@
   ;; FIXME this version will have a flawless: permutation of
   ;; the same buffer name have the same pre-hash of another file.
   ;; Example: to_to_99 and 9_otto_9 will have the same counter, thus the same _hash_ :'(.
-  (let (charlist mvar (counter 0))
-    (setq charlist (string-to-list mstr))
-    (while charlist
-      (setq mvar (car charlist))
-      (setq counter (+ counter mvar))
-      (setq charlist (cdr charlist))
-      )
-    counter
-    )
-  )
+  (let ((counter 0))
+    (dolist (c (string-to-list mstr))
+      (setq counter (+ counter c)))
+    counter))
 
 (defun mmark-set (char)
   "CHAR is the value I must put in the map."
   (interactive "*cChar:")
-  (let (mstr)
-    (setq mstr
-	  (mmark--prehash-string (format "%s_%s" (buffer-name) char))
-	  )
+  (let ((mstr (mmark--prehash-string (format "%s_%s" (buffer-name) char))))
     ;; Override with the same key
     (if (map-contains-key mmark--hashmap mstr)
-	(map-delete mmark--hashmap mstr)
-      )
+	(map-delete mmark--hashmap mstr))
     (map-put mmark--hashmap mstr (point))
-    (message "Set mark `%c` for buffer `%s`." char (buffer-name))
-    )
-  )
+    (message "Set mark `%c` for buffer `%s`." char (buffer-name))))
 
 (defun mmark-get (char)
   "CHAR is the value I must retrieve from the map."
   (interactive "*cChar:")
-  (let (mstr)
-    (setq mstr
-	  (mmark--prehash-string (format "%s_%s" (buffer-name) char))
-	  )
+  (let ((mstr (mmark--prehash-string (format "%s_%s" (buffer-name) char))))
     ;; Get the key, if any
     (if (map-contains-key mmark--hashmap mstr)
 	(goto-char (map-elt mmark--hashmap mstr))
-      (message "Unable to find mark `%c` in buffer `%s`"
-	       char
-	       (buffer-name))
-      )
-    )
-  )
+      (message "Unable to find mark `%c` in buffer `%s`" char (buffer-name)))))
 
-(local-set-key (kbd "C-m s") 'mmark-set)
-(local-set-key (kbd "C-m g") 'mmark-get)
+
+(global-set-key (kbd "C-c m s") 'mmark-set)
+(global-set-key (kbd "C-c m g") 'mmark-get)
 
 (provide 'mmark)
 
